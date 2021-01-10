12cc9ff51e95d4762898eb9fde8d74b81ec29c85
Grahame White <4whitg16@solent.ac.uk>
2021-01-10 20:59:17 +0000

    selecting role now displays required skills

--- public/scripts/roles.js
+++ public/scripts/roles.js
@@ -14,20 +14,46 @@ const roleList = (roles) => {
 };
 
 const skillList = (skills) => {
-  const length = Math.min(10, skills.length);
+  const reqSkills = skills['required skills'].skills;
+  console.log(reqSkills);
+  const length = Math.min(10, reqSkills.length);
 
-  const listStart = `<select id="skill-selector" size="${length}">`;
+  const listStart = `<div class="container">`;
 
   let listBody = '';
-  skills.forEach(element => {
-    listBody += `<option value="${element}">${element}</option>`;
+  reqSkills.forEach(group => {
+    group.skills.forEach(skill => {
+      listBody += skillControls(group.group, skill.skill, skill.level)
+    })
   });
 
-  const listEnd = '</select>';
+  const listEnd = '</div>';
 
   console.log(listStart + listBody + listEnd);
   return listStart + listBody + listEnd;
 };
+
+const skillControls = (group, skill, level) => {
+
+  // <div class="input-group mb-3">
+  //   <div classe="input-group-prepend">
+  //     <span class="input-group-text" id="basic-addon1">@</span>
+  //   </div>
+  //   <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+  // </div>
+
+  let listStart = `<div class="input-group mb-3 overflow-auto">`;
+  listStart += `<div class="input-group-prepend w-75">`;
+
+  let listBody = `<span class="input-group-text w-100">${group} - ${skill}</span>`;
+  listBody += `</div>`;
+  listBody += `<input type="number" class="form-control" name="required[${group}][${skill}]" min="0" max="4" value="${level}" required>`;
+
+  const listEnd = '</div>';
+  console.log(listStart + listBody + listEnd);
+
+  return listStart + listBody + listEnd;
+}
 
 // getRoleList is used from a client side webpage
 /* eslint-disable no-unused-vars */
