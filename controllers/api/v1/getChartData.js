0107f5088cef65fd837b08929a8dbe5398629944
Grahame White <4whitg16@solent.ac.uk>
2021-01-11 19:56:07 +0000

    Added some styling to the radar chart and modified the data to fit the scenario a bit closer

--- controllers/api/v1/getChartData.js
+++ controllers/api/v1/getChartData.js
@@ -1,13 +1,16 @@
 const opts = require('./apiOptions');
+const smoothing = require('svg-radar-chart/smoothing');
 
 const chartSvg = (chart) => {
   return `
 <svg id="skills-chart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
     <style>
         .axis {
+            stroke: #555;
             stroke-width: 0.2;
         }
         .scale {
+            stroke: #999;
             stroke-width: 0.2;
         }
         .shape {
@@ -17,8 +20,11 @@ const chartSvg = (chart) => {
             fill-opacity: 0.6;
         }
         
-        .shape.iphone {
-            fill: #ff00ff;
+        .shape.required {
+            fill: #008800;
+        }
+        .shape.actual {
+            fill: #880088;
         }
     </style>
     ${chart}
@@ -87,9 +93,9 @@ async function getGroups () {
   // Placeholder data, to be replaced with db access
   return {
     // columns
-    battery: 'Battery Capacity',
-    design: 'Design',
-    useful: 'Usefulness'
+    skillGroup1: 'Software',
+    skillGroup2: 'Hardware',
+    skillGroup3: 'Verification'
   };
 }
 
@@ -97,9 +103,8 @@ async function getData () {
   // Placeholder data, to be replaced with db access
   return [
     // data
-    { class: 'iphone', battery: 0.7, design: 1, useful: 0.9 },
-    { class: 'galaxy', battery: 1, design: 0.6, useful: 0.8 },
-    { class: 'nexus', battery: 0.8, design: 0.7, useful: 0.6 }
+    { class: 'required', skillGroup1: 2 / 4, skillGroup2: 2 / 4, skillGroup3: 2 / 4 },
+    { class: 'actual', skillGroup1: 2.9 / 4, skillGroup2: 0.6 / 4, skillGroup3: 3 / 4 }
   ];
 }
 
@@ -119,7 +124,15 @@ async function handleErrors (error) {
 async function getSvgChart (groups, data) {
   // Generate the chart from the gathered elements
   const radar = require('svg-radar-chart');
-  const chart = radar(groups, data, { shapeProps: (data) => ({ className: 'shape ' + data.class }) });
+  const opts = {
+    axes: true,
+    scales: 4,
+    size: 100,
+    smoothing: smoothing(0.5),
+    shapeProps: (data) => ({ className: 'shape ' + data.class })
+  };
+
+  const chart = radar(groups, data, opts);
 
   // Convert the chart to an SVG representation
   const stringify = require('virtual-dom-stringify');
