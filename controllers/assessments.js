6437a1799fc97665af3b38ea5c8119705e891704
Grahame White <4whitg16@solent.ac.uk>
2021-01-13 18:55:04 +0000

    started building the create assessment page

--- controllers/assessments.js
+++ controllers/assessments.js
@@ -8,3 +8,14 @@ exports.view = async (req, res) => {
     });
   }
 };
+
+exports.create = async (req, res) => {
+  console.log(req.session);
+  try {
+    res.render('createAssessment', {});
+  } catch (e) {
+    res.status(404).send({
+      message: 'error rendering page'
+    });
+  }
+};
---
+++ public/scripts/assessments.js
@@ -0,0 +1,113 @@
+const roleList = (roles) => {
+  const listStart = `<label for="role-selector">Role</label>
+    <select id="role-selector" class="form-control">`;
+
+  let listBody = '';
+  roles.forEach(element => {
+    listBody += `<option value="${element.title}">${element.title}</option>`;
+  });
+
+  const listEnd = '</select>';
+
+  return listStart + listBody + listEnd;
+};
+
+const employeeList = (employees) => {
+  const listStart = `<label for="employee-selector">Employee</label>
+    <select id="employee-selector" class="form-control">`;
+
+  let listBody = '';
+  employees.forEach(element => {
+    listBody += `<option value="${element.email}">${element.name}</option>`;
+  });
+
+  const listEnd = '</select>';
+
+  return listStart + listBody + listEnd;
+};
+
+// const skillList = (skills) => {
+//   const reqSkills = skills['required skills'].skills;
+//   console.log(reqSkills);
+//
+//   let listStart = '<div class="container">';
+//   listStart += '<form id="skills-form" name="skills-form" method="post">';
+//
+//   let listBody = '';
+//   let skillNo = 0;
+//   reqSkills.forEach(group => {
+//     group.skills.forEach(skill => {
+//       listBody += skillControls(group.group, skill.skill, skill.level, skillNo);
+//       skillNo++;
+//     });
+//   });
+//
+//   const listEnd = '</form></div>';
+//
+//   return listStart + listBody + listEnd;
+// };
+
+// const skillControls = (group, skill, level, skillNo) => {
+//   let listStart = '<div class="input-group mb-1 overflow-auto">';
+//   listStart += '<div class="input-group-prepend w-75">';
+//
+//   let listBody = `<span class="input-group-text w-100">${group} - ${skill}</span>`;
+//   listBody += '</div>';
+//   listBody += `<input type="number" class="form-control" name="requiredSkill[${skillNo}][level]" min="0" max="4" value="${level}" required>`;
+//   listBody += `<input type="hidden" name="requiredSkill[${skillNo}][group]" value="${group}">`;
+//   listBody += `<input type="hidden" name="requiredSkill[${skillNo}][skill]" value="${skill}">`;
+//
+//   const listEnd = '</div>';
+//
+//   return listStart + listBody + listEnd;
+// };
+
+// onLoad is used from a client side webpage
+/* eslint-disable no-unused-vars */
+async function onLoad () {
+  /* eslint-enable no-unused-vars */
+  await getRoleList();
+  await getEmployeeList();
+}
+
+// getRoleList is used from a client side webpage
+/* eslint-disable no-unused-vars */
+async function getRoleList () {
+  /* eslint-enable no-unused-vars */
+  const apiUrl = '/api/v1/role';
+
+  const roleDomRef = document.querySelector('#role-list');
+  try {
+    console.log('requesting roles list');
+    const roleRef = await fetch(apiUrl);
+    const roles = await roleRef.json();
+
+    const roleHtml = [];
+    roleHtml.push(roleList(roles));
+    roleDomRef.innerHTML = roleHtml.join('');
+  } catch (e) {
+    console.log(`error using skilled API: ${apiUrl}`);
+    console.log(e);
+  }
+}
+
+// getRoleList is used from a client side webpage
+/* eslint-disable no-unused-vars */
+async function getEmployeeList () {
+  /* eslint-enable no-unused-vars */
+  const apiUrl = '/api/v1/employees';
+
+  const employeeDomRef = document.querySelector('#employee-list');
+  try {
+    console.log('requesting employee list');
+    const employeeRef = await fetch(apiUrl);
+    const employees = await employeeRef.json();
+
+    const employeeHtml = [];
+    employeeHtml.push(employeeList(employees));
+    employeeDomRef.innerHTML = employeeHtml.join('');
+  } catch (e) {
+    console.log(`error using skilled API: ${apiUrl}`);
+    console.log(e);
+  }
+}
